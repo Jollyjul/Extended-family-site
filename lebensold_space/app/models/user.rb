47c2799122c1b87e39c9f2eed15505f7c5cfac4a
Julian <julian@lebensold.ca>
2010-06-04 17:40:46 -0400

    User registration added

--- lebensold_space/app/models/user.rb
+++ lebensold_space/app/models/user.rb
@@ -7,12 +7,17 @@ class User < ActiveRecord::Base
   EMAIL_MAX_LENGTH = 50 
   SCREEN_NAME_RANGE = SCREEN_NAME_MIN_LENGTH..SCREEN_NAME_MAX_LENGTH 
   PASSWORD_RANGE = PASSWORD_MIN_LENGTH..PASSWORD_MAX_LENGTH 
-  
+
+  # Text box sizes for display in the views 
+  SCREEN_NAME_SIZE = 20 
+  PASSWORD_SIZE = 10 
+  EMAIL_SIZE = 30 
+
   validates_uniqueness_of :screen_name, :email 
   validates_length_of :screen_name, :within => SCREEN_NAME_RANGE
   validates_length_of :password, :within => PASSWORD_RANGE 
   validates_length_of :email, :maximum => EMAIL_MAX_LENGTH 
-  
+
   validates_format_of :screen_name, 
                       :with => /^[A-Z0-9_]*$/i, 
                       :message => "must contain only letters, " + 
@@ -20,4 +25,5 @@ class User < ActiveRecord::Base
   validates_format_of :email, 
                       :with => /^[A-Z0-9._%-]+@([A-Z0-9-]+\.)+[A-Z]{2,4}$/i, 
                       :message => "must be a valid email address"
-end 
+
+end
